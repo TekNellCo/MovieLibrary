17e3449a64d93495ec094a74f01369c6ceb7aaf5
Nelson <129366078+TekNellCo@users.noreply.github.com>
2024-11-10 20:07:35 -0500

    carousel containers/card creators for home

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import './styles.css';
-import { greeting } from './greeting.js';
+import { homeBuilder } from './home.js';
 
-console.log(greeting);
+export const mainBody = document.querySelector('.mainBody');
+const home = document.querySelector('.home');
+const watchList = document.querySelector('.watchList');
+let page = 'home';
+
+homeBuilder();
+
+home.addEventListener('click', () => {
+  if (page === 'home') {
+  } else if (page !== 'home') {
+    clearMainBody();
+    homeBuilder();
+    page = 'home';
+  }
+});
+
+watchList.addEventListener('click', () => {
+  if (page === 'watchList') {
+  } else if (page !== 'watchList') {
+    clearMainBody();
+    console.log('watchlist');
+    page = 'watchlist';
+  }
+});
+
+function clearMainBody() {
+  mainBody.innerHTML = '';
+}
